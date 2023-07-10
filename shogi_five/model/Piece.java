5c9473ce197b2b7d79dd1c3448c7fc8815c15b16
T1210Taichi <T1201Taichi@gmail.com>
2023-07-10 07:37:12 +0000

    change dir structure

--- shogi_five/model/Piece.java
+++ shogi_five/model/Piece.java
@@ -1,17 +1,27 @@
 package model.piece;
 
-import model.board.Board;
+import model.Board;
 
-public class Piece {
+/**
+ * 将棋の駒を表す抽象クラス
+ */
+
+public abstract class Piece {
     private int pieceClass;//駒の種類
     private int position;//駒の現在地
     private boolean promote;//成りかどうか
     private Selection[] availableMoves;//移動可能なリスト
 
+    /*
+    * コンストラクタ
+    */
     public Piece(int pieceClass, int position, Selection[] availableMoves) {
         this.pieceClass = pieceClass;
         this.position = position;
         this.promote = false;
         this.availableMoves = availableMoves;
     }
+
+    //動きを定義する
+    //public abstract int[] move();
 }
