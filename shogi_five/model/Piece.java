1887e2ad375f91a196fb3dc4f969fc79ebe5fcc8
T1210Taichi <t1201taichi@gmail.com>
2023-07-14 17:27:48 +0000

    change information

--- shogi_five/model/Piece.java
+++ shogi_five/model/Piece.java
@@ -10,16 +10,23 @@ public abstract class Piece {
     private int pieceClass;//駒の種類
     private int position;//駒の現在地
     private boolean promote;//成りかどうか
+    private boolean owner;//所有者
 
     /*
     * コンストラクタ
     */
-    public Piece(int pieceClass, int position, Selection[] availableMoves) {
-        this.pieceClass = pieceClass;
-        this.position = position;
-        this.promote = false;
-    }
+    public abstract Piece(int position) {}
+
+    /*
+     * 駒を成らせる
+     */
+    public abstract void promotedPice(){}
+
+    /*
+     * 駒の成りを解除する
+     */
+    public abstract void demotedPice(){}
 
     //動きを定義する
-    public abstract int[] move();
+    public abstract ArrayList<Integer> move();
 }
