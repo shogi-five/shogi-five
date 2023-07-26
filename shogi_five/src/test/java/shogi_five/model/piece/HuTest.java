191db1dfed553379ac723b026f97185f69d58f82
T1210Taichi <T1201Taichi@gmail.com>
2023-07-26 04:19:47 +0000

    誤字の修正

--- shogi_five/src/test/java/shogi_five/model/piece/HuTest.java
+++ shogi_five/src/test/java/shogi_five/model/piece/HuTest.java
@@ -19,63 +19,38 @@ public class HuTest {
      */
     @Test
     public void testMove() {
+        //何もないboardをつくる
         Board board = new Board();
-        ArrayList<Integer> expected = board.getPiece(15).move(board);
+        for (int j = 0; j < 25;j++){
+            board.setPiece(null, j);
+        }
+
+        //銀を置く
+        int pos = 5;
+        board.setPiece(new Hu(pos, true), pos);
+
+        //動く位置に駒を置く
+        //board.setPiece(new Gin(23,true), 23);
+
+        //表示
+        ArrayList<Integer> expected = board.getPiece(pos).move(board);
         for(int i = 0;i < expected.size();i++){
             System.out.println(expected.get(i));
         }
 
     }
-    /*
-     * 駒の動ける場所をテスト(動く場所に自分の駒があるとき)
-     */
-    @Test
-    public void testCannotMove() {
-        Board board = new Board();
-        board.setPiece(new Kin(10, true), 10);//金を歩の前に置く
-        //System.out.println(board.getPiece(10).getClass());
-        ArrayList<Integer> expected = board.getPiece(15).move(board);
-        for(int i = 0;i < expected.size();i++){
-            System.out.println(expected.get(i));
-        }
-
-    }
-    /*
+        /*
      * 駒が成るかテスト
      */
     @Test
     public void testPromotedPiece() {
-        Ou ou = new Ou(0, true);
-        assertFalse(ou.getPromote());
+        Hu hu = new Hu(0, true);
+        assertFalse(hu.getPromote());
 
-        ou.setPromote(true);
-        assertTrue(ou.getPromote());
+        hu.setPromote(true);
+        assertTrue(hu.getPromote());
         
-        ou.setPromote(false);
-        assertFalse(ou.getPromote());
+        hu.setPromote(false);
+        assertFalse(hu.getPromote());
     }
-
-    /*
-     * 駒を成らせて動きをテスト
-     */
-    @Test
-    public void testPromotedAndMove() {
-        Hu hu = new Hu(10, true);//生成
-        ArrayList<Integer> expected1 = new ArrayList<>();
-        expected1.add(5);
-        ArrayList<Integer> actual1 = hu.move();//移動先一覧
-        assertEquals(expected1, actual1);
-        hu.setPosition(actual1.get(0));//移動
-        hu.setPromote(true);//成り
-
-        ArrayList<Integer> expected2 = new ArrayList<>();
-        expected2.add(0);
-        expected2.add(1);
-        expected2.add(6);
-        expected2.add(10);
-
-        ArrayList<Integer> actual2 = hu.move();//移動先一覧
-        
-        assertEquals(expected2, actual2);
-    }    
 }
