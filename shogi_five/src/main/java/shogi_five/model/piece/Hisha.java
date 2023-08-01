17a44d2189dfc151b1abb148ed42201460fef849
takeru115 <takerun115@gmail.com>
2023-08-01 08:06:44 +0000

    飛車と角の作成とテスター

--- shogi_five/src/main/java/shogi_five/model/piece/Hisha.java
+++ shogi_five/src/main/java/shogi_five/model/piece/Hisha.java
@@ -33,8 +33,107 @@ public class Hisha extends Piece{
     public ArrayList<Integer> move(Board board){
         ArrayList<Integer> moveList = new ArrayList<>();
 
+    //成っている・成っていない共通の動き
+    //縦の動き
+         //一番下の段ではないとき,下への移動(プラス方向の移動)を追加
+        for(int i = this.position ; i < 20; i = i + 5){
+            //移動先に自分の駒があるときfor文から抜ける
+            if ((board.getPiece(i + 5) != null) && (board.getPiece(i + 5).getOwner() == this.owner)){
+                break;
+            }
+            //移動先に相手の駒があるとき,移動先を追加してfor文から抜ける
+            if ((board.getPiece(i + 5) != null) && (board.getPiece(i + 5).getOwner() != this.owner)){
+                moveList.add(i + 5);
+                break;
+
+            }
+            moveList.add(i + 5);
+        }
+
+        //一番上の段ではないとき,上への移動(マイナス方向の移動)を追加
+        for (int i = this.position  ; i > 4; i = i - 5){
+            //移動先に自分の駒があるときfor文から抜ける
+            if ((board.getPiece(i - 5) != null) && (board.getPiece(i - 5).getOwner() == this.owner)){
+                break;
+            }
+            //移動先に相手の駒があるとき,移動先を追加してfor文から抜ける
+            if ((board.getPiece(i - 5) != null) && (board.getPiece(i - 5).getOwner() != this.owner)){
+                moveList.add(i - 5);
+                break;
+            }
+            moveList.add(i - 5);
+        }
+
+        //横の動き
+            //右方向(プラス方向)への移動
+            for (int i = 0; i < 4 - (this.position % 5); i++){
+                if ((board.getPiece(this.position + (i + 1)) != null) && (board.getPiece(this.position - (i + 1)).getOwner() == this.owner)){
+                    break;
+                }
+                //移動先に相手の駒があるとき,移動先を追加してfor文から抜ける
+                 if ((board.getPiece(this.position + (i + 1)) != null) && (board.getPiece(this.position - (i + 1)).getOwner() != this.owner)){
+                    moveList.add(this.position + (i + 1));
+                    break;
+                }
+
+                moveList.add(this.position + (i + 1));
+            }
+
+
+            //左方向(マイナス方向の移動)への移動
+            for (int i = 0; i < this.position % 5 ; i++){
+                //移動先に自分の駒があるときfor文から抜ける
+                if ((board.getPiece(this.position - (i + 1)) != null) && (board.getPiece(this.position - (i + 1)).getOwner() == this.owner)){
+                    break;
+                }
+                //移動先に相手の駒があるとき,移動先を追加してfor文から抜ける
+                 if ((board.getPiece(this.position - (i + 1)) != null) && (board.getPiece(this.position - (i + 1)).getOwner() != this.owner)){
+                    moveList.add(this.position - (i + 1));
+                    break;
+                }
+
+                moveList.add(this.position - (i + 1));
+            }
+
+        //成っているときの動き
+        if (this.promote == true){
+
+            //左上の動き(マイナス6方向)
+            if ((this.position % 5 !=  0) && (this.position > 5)){
+                //自分の駒がないとき
+                if ((board.getPiece(this.position - 6)  == null) || (board.getPiece(this.position - 6).getOwner() != this.owner)){
+                    moveList.add(this.position - 6);
+                }
+            }
+
+             //右上の動き(マイナス4方向)
+            if ((this.position % 5 !=  4) && (this.position > 5)){
+                //自分の駒がないとき
+                if ((board.getPiece(this.position - 4)  == null) || (board.getPiece(this.position - 4).getOwner() != this.owner)){
+                    moveList.add(this.position - 4);
+                }
+            }
+
+            //右下の動き(プラス6方向)
+            if ((this.position % 5 !=  4) && (this.position < 20)){
+                //自分の駒がないとき
+                if ((board.getPiece(this.position + 6)  == null) || (board.getPiece(this.position + 6).getOwner() != this.owner)){
+                    moveList.add(this.position + 6);
+                }
+            }
+
+            //左下の動き(プラス4方向)
+            if ((this.position % 5 !=  0) && (this.position < 20)){
+                //自分の駒がないとき
+                if ((board.getPiece(this.position + 4)  == null) || (board.getPiece(this.position + 4).getOwner() != this.owner)){
+                    moveList.add(this.position + 4);
+                }
+            }
+        }
+
         return moveList;
     }
+
 
 
     /*
@@ -74,7 +173,7 @@ public class Hisha extends Piece{
      */
     public void setOwner(boolean owner){
         this.owner = owner;
-    }    
+    }
     /*
      * pieceClassのゲッター
      */
